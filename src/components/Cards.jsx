21d1458545e2257e3afc78ff9914e2bbe0d50ca6
DannaMish <zachary_sex30@hotmail.com>
2023-01-19 02:04:52 -0500

    estilo

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,22 @@
 import Card from './Card';
+import style from "./Cards.module.css";
 
 
 export default function Cards(props) {
    const { characters } = props;
    return (
       <div>
-         {characters.map(character => (
-            <Card
-            name={character.name} 
-            species={character.species} 
-            gender={character.gender} 
-            image={character.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-            />
-         ))}
+         <ul className={style.cards}>
+            {characters.map(character => (
+               <Card
+               name={character.name} 
+               species={character.species} 
+               gender={character.gender} 
+               image={character.image}
+               onClose={() => window.alert('Emulamos que se cierra la card')}
+               />
+            ))}
+         </ul>
       </div>
    );
 }
